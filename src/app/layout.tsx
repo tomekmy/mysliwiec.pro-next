6607acf416b9993a5e6ecbcee23fcaf596c11c83
Tomasz Myśliwiec <tomasz.mysliwiec@execon.pl>
2023-10-12 12:29:08 +0200

    Add prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,23 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Jura } from 'next/font/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Jura } from "next/font/google";
 
-const jura = Jura({ subsets: [ 'latin' ] });
+const jura = Jura({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Tomasz Myśliwiec - Front-end Developer',
-  description: 'Nazywam się Tomasz Myśliwiec i jestem Front-end Developerem. Zajmuję kodowaniem stron i aplikacji internetowych.',
+  title: "Tomasz Myśliwiec - Front-end Developer",
+  description:
+    "Nazywam się Tomasz Myśliwiec i jestem Front-end Developerem. Zajmuję kodowaniem stron i aplikacji internetowych.",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="en">
-      <body
-        className={jura.className}>{children}</body>
+    <html lang="pl">
+      <body className={jura.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,19 @@
-'use client';
+"use client";
 
-import { Textillate } from 'textillate-react';
+import { Textillate } from "textillate-react";
 
 const Home = () => {
   return (
-    <main
-      className="grid grid-cols-[6%_300px_1fr_6%] min-h-screen">
-      <div
-        className="grid grid-rows-[200px_1fr_80px]">
+    <main className="grid grid-cols-[6%_300px_1fr_6%] min-h-screen">
+      <div className="grid grid-rows-[200px_1fr_80px]">
         <div>left</div>
         <div>left</div>
         <div>left</div>
       </div>
-      <div
-        className="grid grid-rows-[200px_1fr_80px] pl-2.5">
-        <div
-          className="pt-3">
-          <h1
-            className="text-4xl">Tomasz Myśliwiec</h1>
-          <h2
-            className="text-lg">Front-End Developer</h2>
+      <div className="grid grid-rows-[200px_1fr_80px] pl-2.5">
+        <div className="pt-3">
+          <h1 className="text-4xl">Tomasz Myśliwiec</h1>
+          <h2 className="text-lg">Front-End Developer</h2>
         </div>
         <ul>
           <li>START</li>
@@ -29,20 +23,20 @@ const Home = () => {
         </ul>
         <div>bottom</div>
       </div>
-      <div
-        className="grid grid-rows-[1fr_80px]">
-        <Textillate
-          className="text-2xl"
-          option={{
-            in: { effect: 'fadeInRight', shuffle: true },
-          }}
-        >
-          Witaj.
-          Jestem Front-End Developerem.
-          Zajmuję się kodowaniem stron i aplikacji internetowych.
-        </Textillate>
-        <div
-          className="text-right">language</div>
+      <div className="grid grid-rows-[1fr_80px]">
+        <div className="text-2xl">
+          {typeof window !== "undefined" && (
+            <Textillate
+              option={{
+                in: { effect: "fadeInRight", shuffle: true },
+              }}
+            >
+              Witaj. Jestem Front-End Developerem. Zajmuję się kodowaniem stron
+              i aplikacji internetowych.
+            </Textillate>
+          )}
+        </div>
+        <div className="text-right">language</div>
       </div>
       <div>right</div>
     </main>
