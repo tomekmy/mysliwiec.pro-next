b5a126a7feb0d89917aa1cf54c9791676b8df44d
Tomasz Myśliwiec <tomasz.mysliwiec@execon.pl>
2023-10-16 08:58:33 +0200

    Refactor menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -2,34 +2,42 @@
 
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+const ROUTES = [
+  {
+    name: "START",
+    route: "/",
+  },
+  {
+    name: "O MNIE",
+    route: "/about",
+  },
+  {
+    name: "PORTFOLIO",
+    route: "/portfolio",
+  },
+  {
+    name: "KONTAKT",
+    route: "/contact",
+  },
+];
 
 const Menu = () => {
+  const pathname = usePathname();
+
   return (
     <ul className="text-2xl hover:[&>li]:text-red-700 [&>li]:w-fit [&>li]:my-1.5">
-      <motion.li
-        whileHover={{ scale: 1.1 }}
-        transition={{ type: "spring", stiffness: 400, damping: 5 }}
-      >
-        <Link href="/">START</Link>
-      </motion.li>
-      <motion.li
-        whileHover={{ scale: 1.1 }}
-        transition={{ type: "spring", stiffness: 400, damping: 5 }}
-      >
-        <Link href="/about">O MNIE</Link>
-      </motion.li>
-      <motion.li
-        whileHover={{ scale: 1.1 }}
-        transition={{ type: "spring", stiffness: 400, damping: 5 }}
-      >
-        <Link href="/portfolio">PORTFOLIO</Link>
-      </motion.li>
-      <motion.li
-        whileHover={{ scale: 1.1 }}
-        transition={{ type: "spring", stiffness: 400, damping: 5 }}
-      >
-        <Link href="/contact">KONTAKT</Link>
-      </motion.li>
+      {ROUTES.map((link) => (
+        <motion.li
+          key={link.route}
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: "spring", stiffness: 400, damping: 5 }}
+          className={pathname == link.route ? "text-red-700" : ""}
+        >
+          <Link href={link.route}>{link.name}</Link>
+        </motion.li>
+      ))}
     </ul>
   );
 };
